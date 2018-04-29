a563b85f06f6081f89691cfe00abc6838d327a9f
Lucas Calebe <calebematos@gmail.com>
2018-04-29 20:58:41 -0300

    Adicionando serviço de mensagem

--- algamoney-ui/src/app/app.module.ts
+++ algamoney-ui/src/app/app.module.ts
@@ -1,14 +1,12 @@
-import { PessoasService } from './pessoas/pessoas.service';
-import { LancamentoService } from './lancamentos/lancamento.service';
-import { CoreModule } from './core/core.module';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { CoreModule } from './core/core.module';
 import { PessoasModule } from './pessoas/pessoas.module';
 import { LancamentosModule } from './lancamentos/lancamentos.module';
-import { HttpModule } from '@angular/http';
 
 
 
@@ -26,7 +24,7 @@ import { HttpModule } from '@angular/http';
     LancamentosModule,
     PessoasModule,
   ],
-  providers: [LancamentoService, PessoasService],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- algamoney-ui/src/app/core/core.module.ts
+++ algamoney-ui/src/app/core/core.module.ts
@@ -1,12 +1,41 @@
-import { NgModule } from '@angular/core';
+import { NgModule, LOCALE_ID } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { registerLocaleData } from '@angular/common';
+import localePt from '@angular/common/locales/pt';
+
+import { ConfirmationService } from 'primeng/api';
+import { ConfirmDialogModule } from 'primeng/confirmdialog';
+import { ToastyModule } from 'ng2-toasty';
+
+import { CategoriaService } from './../categorias/categoria.service';
+import { PessoasService } from './../pessoas/pessoas.service';
+import { LancamentoService } from './../lancamentos/lancamento.service';
 import { NavbarComponent } from './navbar/navbar.component';
+import { ErrorHandlerService } from './error-handler.service';
+
+registerLocaleData(localePt, 'pt-BR');
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+
+    ToastyModule.forRoot(),
+    ConfirmDialogModule,
   ],
-  declarations: [ NavbarComponent ],
-  exports: [ NavbarComponent ]
+  declarations: [NavbarComponent],
+  exports: [
+    NavbarComponent,
+    ToastyModule,
+    ConfirmDialogModule
+  ],
+  providers: [
+    LancamentoService,
+    PessoasService,
+    CategoriaService,
+    ErrorHandlerService,
+
+    ConfirmationService,
+    { provide: LOCALE_ID, useValue: 'pt-BR' }
+  ]
 })
 export class CoreModule { }
