977ebf9231dce6b5cc339f597b05507298d993aa
Lucas Calebe <calebematos@gmail.com>
2018-05-06 22:01:16 -0300

    ajuste

--- algamoney-ui/src/app/seguranca/logout.service.ts
+++ algamoney-ui/src/app/seguranca/logout.service.ts
@@ -1,5 +1,6 @@
-import { AuthHttp } from 'angular2-jwt';
 import { Injectable } from '@angular/core';
+
+import { AuthHttp } from 'angular2-jwt';
 
 import { environment } from './../../environments/environment';
 import { AuthService } from './auth.service';
