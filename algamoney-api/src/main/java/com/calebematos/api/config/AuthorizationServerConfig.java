1b567159955bdd16fc48b39f87371a4e3180c56c
Lucas Calebe <calebematos@gmail.com>
2018-08-26 21:39:04 -0300

    Atualizando Spring boot 2

--- algamoney-api/src/main/java/com/calebematos/api/config/AuthorizationServerConfig.java
+++ algamoney-api/src/main/java/com/calebematos/api/config/AuthorizationServerConfig.java
@@ -7,9 +7,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.provider.token.TokenEnhancerChain;
 import org.springframework.security.oauth2.provider.token.TokenStore;
@@ -20,17 +20,19 @@ import com.calebematos.api.config.token.CustonTokenEnhancer;
 
 @Profile("oauth-security")
 @Configuration
-@EnableAuthorizationServer
 public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter{
 
 	@Autowired
 	private AuthenticationManager authenticationManager;
 	
+	@Autowired
+	private UserDetailsService userDetailsService;
+	
 	@Override
 	public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
 		clients.inMemory()
 			.withClient("angular")
-			.secret("@ngul@r0")
+			.secret("$2a$10$RAxEjJgp0RZ7rVV8fNLMQer//0M.JA2Qir1TuMzGC3c.RDBOoSyOu") //@ngul@r0
 			.scopes("read", "write")
 			.authorizedGrantTypes("password","refresh_token")
 			.accessTokenValiditySeconds(1800)
@@ -48,6 +50,7 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 			.tokenStore(tokenStore())
 			.tokenEnhancer(tokenEnhancerChain)
 			.reuseRefreshTokens(false)
+			.userDetailsService(userDetailsService)
 			.authenticationManager(authenticationManager);
 	}
 
